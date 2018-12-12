9f991e1a2e23dab2fa36c5398e5480e34e003a4d
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-12 11:23:22 +0530

    Video Play/Pause enabled

---
+++ Custom Video Player/scripts.js
@@ -0,0 +1,26 @@
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer');
+
+const toggleButton = player.querySelector('.toggle');
+
+//console.log(video);
+
+let isPlaying = false;
+
+function toggle(){
+	if(isPlaying){
+		video.pause();
+		toggleButton.textContent = '►';
+		isPlaying = false;
+	}
+	else{
+		video.play();
+		toggleButton.textContent = '❚ ❚';
+		isPlaying = true;
+	}
+}
+
+video.addEventListener('click',toggle);
+ 
+
