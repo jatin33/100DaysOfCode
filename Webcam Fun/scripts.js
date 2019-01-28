0c6a7528ee2c0bc41b82f0bcd77e3597dade2878
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-28 21:13:45 +0530

    video stream enabled

--- Webcam Fun/scripts.js
+++ Webcam Fun/scripts.js
@@ -3,3 +3,23 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+const streaming = false;
+
+	// used to get video stream from webcam using promises
+	navigator.getUserMedia({video : true,audio : false}).
+	then((stream)=>{
+		video.srcObject = stream;
+		video.play();
+	}).
+	catch((err)=>{
+		console.log(err);
+	});
+
+
+
+
+function takePhoto(){
+
+}
