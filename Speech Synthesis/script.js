5305d5e17c6ab5ddae459a9c91661ebdbf25e6f8
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-02-07 11:24:46 +0530

    Voice Dropdown feature completed

--- Speech Synthesis/script.js
+++ Speech Synthesis/script.js
@@ -1,6 +1,21 @@
   const msg = new SpeechSynthesisUtterance();
   let voices = [];
-  const voicesDropdown = document.querySelector('[name="voice"]');
+  
+  const voicesDropdown = document.querySelector('#voices');
+  console.log(voicesDropdown);
+  
+
+  const voiceschanged = () => {
+  		speechSynthesis.getVoices().forEach(voice => voices.push(voice.name + '-' + voice.lang));		
+  		for(let i = 0; i < voices.length; i++){
+			const option = document.createElement('option');
+			option.textContent = voices[i];
+			option.value = voices[i];
+			voicesDropdown.appendChild(option);
+	}
+}
+	speechSynthesis.onvoiceschanged = voiceschanged;
+
   const options = document.querySelectorAll('[type="range"], [name="text"]');
   console.log(options);
   const speakButton = document.querySelector('#speak');
