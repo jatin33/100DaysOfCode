dbad381cf847e93e567181f554795d42a08f8443
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-02-27 11:21:44 +0530

    Dropdown appears without background

--- Stripe Follow Along Nav/script.js
+++ Stripe Follow Along Nav/script.js
@@ -1,14 +1,13 @@
 const navTitles = document.querySelectorAll('.cool > li');
-
+const background = document.querySelector('.dropdownBackground');
 
 function handleEnterElement(){
-console.log('enters');
-this.classList.add('.dropdown');
+this.classList.add('trigger-enter');        
+  setTimeout(()=> this.classList.add('trigger-enter-active'),150);  
 }
 
 function handleLeaveElement(){
-console.log('leaves');
-this.classList.remove('.dropdown');
+this.classList.remove('trigger-enter','trigger-enter-active');
 }
 
 navTitles.forEach( navTitle => navTitle.addEventListener('mouseenter',handleEnterElement));
