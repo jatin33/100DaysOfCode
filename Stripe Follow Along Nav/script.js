fb8280caad54e3bcec510f3c1e50621680c1c5cb
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-03-05 09:05:32 +0530

    Dropdown appears in corner

--- Stripe Follow Along Nav/script.js
+++ Stripe Follow Along Nav/script.js
@@ -4,10 +4,12 @@ const background = document.querySelector('.dropdownBackground');
 function handleEnterElement(){
 this.classList.add('trigger-enter');        
   setTimeout(()=> this.classList.add('trigger-enter-active'),150);  
+  background.classList.add('open');
 }
 
 function handleLeaveElement(){
 this.classList.remove('trigger-enter','trigger-enter-active');
+background.classList.remove('open');
 }
 
 navTitles.forEach( navTitle => navTitle.addEventListener('mouseenter',handleEnterElement));
