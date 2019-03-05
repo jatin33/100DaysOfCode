00e9b7a505a71d7d244006fc564b82b375c09311
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-03-05 10:38:26 +0530

    Project completed

--- Stripe Follow Along Nav/script.js
+++ Stripe Follow Along Nav/script.js
@@ -1,10 +1,21 @@
 const navTitles = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
 
 function handleEnterElement(){
 this.classList.add('trigger-enter');        
-  setTimeout(()=> this.classList.add('trigger-enter-active'),150);  
+  setTimeout(()=> this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'),150);  
   background.classList.add('open');
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+  // console.log(dimensions);
+  console.log(dropdown);
+  
+background.style.setProperty('width',`${dropdownCoords.width}px`);
+background.style.setProperty('height',`${dropdownCoords.height}px`);
+background.style.setProperty('left',`${dropdownCoords.left - navCoords.left}px`);
+background.style.setProperty('top',`${dropdownCoords.top - navCoords.top}px`);
 }
 
 function handleLeaveElement(){
