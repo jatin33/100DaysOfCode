4f02fefb5ccf68ec5e001a84c3279efeb15f992e
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-03-10 20:53:41 +0530

    Project completed

--- Click and Drag/script.js
+++ Click and Drag/script.js
@@ -1,4 +1,30 @@
-const cards = document.querySelectorAll('.item');
+const slider = document.querySelector('.items');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;// offsetLeft gives distance of childs left border box to parent border box
+    scrollLeft = slider.scrollLeft;
+  });
+  
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
 
-console.log(cards);
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
 
+  slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const distanceDragged = (x - startX) * 4;
+    console.log(distanceDragged);
+    slider.scrollLeft = scrollLeft - distanceDragged;
+  });
