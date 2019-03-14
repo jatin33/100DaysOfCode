e655d3481ef1199e1b94a02441c87bd110de5221
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-03-14 10:53:53 +0530

    Displays current time successfully

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,21 +1,38 @@
 const timerControls = document.querySelector('.timer__controls');
 const displayTimeLeft = document.querySelector('.display__time-left');
-const timeEntered = document.querySelector('#minutesControl');
-let time = 0;
-function setTime(e){
-    if(e.target == 'input'){
-        time = timeEntered.nodeValue;
-    }else{
-        time = e.target.dataset.time;
-    }
-    console.log(time);
-    
+const form = document.querySelector('#custom');
+let countdown;
+
+
+function setTime(e){  
+      let seconds = e.target.dataset.time;
+      timer(seconds);
 }
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    let seconds = this.minutes * 60;
+    timer(seconds);
+    this.reset();
+});
 
 timerControls.addEventListener('click',setTime);
 
-function timer(){
-    let min = time / 60;
+function timer(seconds){
+    clearInterval(countdown);
+    
+    countdown = setInterval(()=>{
+        if(seconds === 0){
+            clearInterval(countdown);
+            return;
+        }
+        displayCurrentTime(seconds);
+        seconds--;
+    },1000);
 }
 
-setInterval(timer,1000);
+function displayCurrentTime(seconds){
+let min = Math.floor(seconds / 60);
+let secs = seconds % 60;
+displayTimeLeft.innerHTML = `${min}:${secs < 10 ? '0' : ''}${secs}`;
+}
